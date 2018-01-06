f16c8a7d2d928fd45b4f492ffc7f66d0229aafe0
sujeeth <avvaru79.java@gmail.com>
2018-01-06 13:09:30 -0500

    Initial Commit

---
+++ src/model/Student.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Student {
+
+    private String FirstName;
+    private int Age;
+    private String LastName;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+}
